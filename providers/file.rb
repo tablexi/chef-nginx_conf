ac411bb90728650773f51c7b663479d07a0c1c01
Lloyd Philbrook <lloyd@firebellydesign.com>
2012-10-10 13:10:02 +0800

    fixed bugs with new_resource defaults

--- providers/file.rb
+++ providers/file.rb
@@ -1,14 +1,16 @@
 require 'chef/util/file_edit'
+require 'yaml'
 
 action :create do
-  locations = node['nginx_conf']['locations'].merge(new_resource.locations)
-  options = node['nginx_conf']['options'].merge(new_resource.options)  
+  listen = new_resource.listen || node['nginx_conf']['listen']
+  locations = node['nginx_conf']['locations'].to_hash.merge(new_resource.locations)
+  options = node['nginx_conf']['options'].to_hash.merge(new_resource.options)
   server_name = new_resource.server_name || new_resource.name
-  try_files = options['try_files']
+  try_files = options.delete('try_files')
   type = :dynamic
 
   if new_resource.socket
-    locations['@proxy']['proxy_pass'] = "#{node['nginx_conf']['pre_socket']}#{new_resource.socket}"
+    locations['@proxy']['proxy_pass'] = node['nginx_conf']['pre_socket'] + new_resource.socket
   elsif !locations['@proxy']['proxy_pass']
     type = :static
   end
@@ -22,42 +24,32 @@ action :create do
   template "#{node['nginx']['dir']}/sites-available/#{new_resource.name}" do
     owner "root"
     group "root"
-    mode "0755"
+    mode "755"
     source new_resource.template
     cookbook new_resource.cookbook
     variables(
       :block =>  new_resource.block,
       :options =>  options,
+      :listen => listen,
       :locations =>  locations,
       :root =>  new_resource.root,
       :server_name => server_name,
       :try_files => try_files,
       :type =>  type
     )
-    notifies :create, resources(:link => "#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}"), :immediately
+  end
+
+  execute "test-nginx-conf" do
+    command "nginx -t"
+    action :nothing
+    notifies :restart, resources(:service => "nginx"), new_resource.reload
   end
 
   link "#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}" do
-    action :nothing
     to "#{node['nginx']['dir']}/sites-available/#{new_resource.name}"
-    notifies :restart, resources(:service => "nginx"), new_resource.reload
-    only_if do
-      # This seems hacky, but how else can you test the conf before creating the symlink.
-      FileUtils.mkdir_p('/tmp/nginx')
-      FileUtils.cp([File.join(node['nginx']['dir'], 'nginx.conf'), File.join(node['nginx']['dir'], 'sites-available', new_resource.name)], '/tmp/nginx')
-
-      nginx_conf = Chef::Util::FileEdit.new("/tmp/nginx/nginx.conf")
-      nginx_conf.search_file_replace_line(/include .*\/conf.d\/\*.conf;/, "include /tmp/nginx/#{new_resource.name};")
-      nginx_conf.search_file_delete_line(/include .*\/sites-enabled\/\*;/)
-      nginx_conf.write_file
-
-      `nginx -t -c /tmp/nginx/nginx.conf`;  result=$?.success?
-
-      FileUtils.remove_dir('/tmp/nginx')
-
-      result
-    end
+    notifies :run, resources(:execute => "test-nginx-conf"), :immediately
   end
+
   new_resource.updated_by_last_action(true)
 end
 
@@ -71,6 +63,7 @@ action :delete do
   template "#{node['nginx']['dir']}/sites-available/#{new_resource.name}" do
     action :delete
   end
+
   new_resource.updated_by_last_action(true)
 end
 
@@ -80,5 +73,6 @@ action :disable do
     to "#{node['nginx']['dir']}/sites-available/#{new_resource.name}"
     notifies :restart, resources(:service => "nginx"), new_resource.reload
   end
+
   new_resource.updated_by_last_action(true)
 end
--- resources/file.rb
+++ resources/file.rb
@@ -1,18 +1,18 @@
-action :create, :delete, :disable
+actions :create, :delete, :disable
 
 default_action :create
 
-attribute :cookbook, :kind_of => String, :default => "nginx_conf" #Cookbook to find template
-attribute :block, :kind_of => [String, Array] # Include additional code
-attribute :listen, :kind_of => String, :default => node['nginx_conf']['listen']  # Listening port, ip, etc.
-attribute :locations, :kind_of => Hash, :default => {} # Locations to include.
+attribute :block, :kind_of => [String, Array, NilClass] # Include additional code
+attribute :cookbook, :kind_of => [String, NilClass], :default => "nginx_conf" #Cookbook to find template
+attribute :listen, :kind_of => [String, NilClass]  # Listening port, ip, etc.
+attribute :locations, :kind_of => [Hash, NilClass], :default => {} # Locations to include.
 attribute :name, :name_attribute => true, :kind_of => String
-attribute :options, :kind_of => Hash, :default => {} # Key value pairs of options to include in the Server body.
-attribute :reload, :default => :delayed # How soon should we restart nginx.
-attribute :root, :kind_of => String # Server root
-attribute :server_name, :kind_of => String # Server name if different then the name attribute.
-attribute :socket, :kind_of => String # Path to socket file.
-attribute :template, :kind_of => String, :default => "conf.erb" # Template to use.
+attribute :options, :kind_of => [Hash, NilClass], :default => {} # Key value pairs of options to include in the Server body.
+attribute :reload, :kind_of => [Symbol, NilClass], :default => :delayed # How soon should we restart nginx.
+attribute :root, :kind_of => [String, NilClass] # Server root
+attribute :server_name, :kind_of => [String, NilClass] # Server name if different then the name attribute.
+attribute :socket, :kind_of => [String, NilClass] # Path to socket file.
+attribute :template, :kind_of => [String, NilClass], :default => "conf.erb" # Template to use.
 
 def initialize(*args)
   super

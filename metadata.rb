37f47a7aaf46ddb9966d2ec74ce33d64a1c16059
Lloyd Philbrook <lloyd@firebellydesign.com>
2012-10-10 13:31:49 +0800

    changed to GPLv3 license

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Firebelly Design"
 maintainer_email "lloyd@firebellydesign.com"
-license          "All rights reserved"
+license          "GNU Public License 3.0"
 description      "Installs/Configures nginx_conf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"

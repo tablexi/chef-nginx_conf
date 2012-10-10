8b0bb487d20164f00633930ada972f0fe9e83e73
Lloyd Philbrook <lloyd@firebellydesign.com>
2012-10-10 13:12:03 +0800

    added platform and cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 maintainer       "Firebelly Design"
-maintainer_email "llod@firebellydesign.com"
+maintainer_email "lloyd@firebellydesign.com"
 license          "All rights reserved"
 description      "Installs/Configures nginx_conf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-
+depends 'nginx'
 supports 'ubuntu'

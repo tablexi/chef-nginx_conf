09d0143f84516b11cc0292fef08c7d4f3f8d3f35
Lloyd Philbrook <lloyd@firebellydesign.com>
2012-10-23 16:54:52 +0800

    fixed nginx restart issue

--- providers/file.rb
+++ providers/file.rb
@@ -7,17 +7,33 @@ action :create do
   options = node['nginx_conf']['options'].to_hash.merge(new_resource.options)
   server_name = new_resource.server_name || new_resource.name
   type = :dynamic
+  proxy_pass = false
 
-  if new_resource.socket
+  if type == :dynamic
+    locations.each do |name, location|
+      proxy_pass = true if location['proxy_pass']
+      if options['try_files']
+        options['try_files'] << " #{name}" if name.index('@') == 0
+      end
+    end
+  end
+
+  if new_resource.socket && locations.has_key?('/')
     locations['/']['proxy_pass'] = node['nginx_conf']['pre_socket'] + new_resource.socket
-  elsif !locations['/']['proxy_pass']
+  elsif !proxy_pass
     type = :static
   end
 
-  if type == :dynamic && options['try_files']
-    locations.each do |name, location|
-      options['try_files'] << " #{name}" if name.index('@') == 0
-    end
+  execute "test-nginx-conf" do
+    command "nginx -t"
+    action :nothing
+    notifies :restart, resources(:service => "nginx"), new_resource.reload
+  end
+
+  link "#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}" do
+    action :nothing
+    to "#{node['nginx']['dir']}/sites-available/#{new_resource.name}"
+    notifies :run, resources(:execute => "test-nginx-conf"), :immediately
   end
 
   template "#{node['nginx']['dir']}/sites-available/#{new_resource.name}" do
@@ -35,17 +51,7 @@ action :create do
       :server_name => server_name,
       :type =>  type
     )
-  end
-
-  execute "test-nginx-conf" do
-    command "nginx -t"
-    action :nothing
-    notifies :restart, resources(:service => "nginx"), new_resource.reload
-  end
-
-  link "#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}" do
-    to "#{node['nginx']['dir']}/sites-available/#{new_resource.name}"
-    notifies :run, resources(:execute => "test-nginx-conf"), :immediately
+    notifies :create, resources(:link => "#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}"), :immediately
   end
 
   new_resource.updated_by_last_action(true)

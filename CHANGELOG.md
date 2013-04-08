f1094510489a4dc1a7d5406486c8c4c1956df0e2
Lloyd Philbrook <lloyd@firebellydesign.com>
2013-04-08 18:47:51 +0800

    added Array to options resource value

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
-## v0.6.0:
+## v0.0.6:
 
 * Use nginx user and group attribute for template owner.
 * [#4] Add ssl support.
+* Allow options to use Hash or Array.
--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "lloyd@firebellydesign.com"
 license          "GNU Public License 3.0"
 description      "Installs/Configures nginx_conf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "0.0.6"
 depends 'nginx'
 supports 'ubuntu'

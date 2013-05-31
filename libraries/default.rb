609db3be6bf2bec617a3dd9653e6e690d3a72308
Lloyd Philbrook <lloyd@firebellydesign.com>
2013-05-31 15:24:19 +0800

    moved template parsing to library/default

---
+++ libraries/default.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: nginx_conf
+# Library:: default
+#
+# Copyright 2012, Firebelly Design
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+def nginx_conf_options(options, level=0, type=false) do
+  indent = '  ' * level
+  output = []
+  options.each do |option,value|
+    if type == 'location'
+      output << <<-CFG
+#{indent}location #{option} {
+#{nginx_conf_locations(value,level+1)}
+#{indent}}
+
+CFG
+    elsif type == 'if'
+      output << <<-CFG
+#{indent}if (#{option}) {
+#{nginx_conf_locations(value,level+1)}
+#{indent}}
+
+CFG
+    else
+      if value.kind_of?(Hash)
+        if ['locations', 'if'].include? option
+          output << nginx_conf_options(value, level, option)
+        else
+          value.each do |k,v|
+            output << "#{indent}#{option} #{k} #{v};"
+          end
+        end
+      elsif value.kind_of?(Array)
+        value.each do |v|
+          output << nginx_conf_options({option => v}, level)
+        end
+      else
+        if option == 'block'
+          output << "#{indent}#{value};"
+        else
+          output << "#{indent}#{option} #{value};"
+        end
+      end
+    end
+  end
+  output.join("\n")
+end

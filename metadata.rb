548f0706dae05c1b151906111228c48a1fcd0fa3
Lloyd Philbrook <lloyd@tablexi.com>
2016-09-23 09:26:40 -0500

    Remove explicit dependency on nginx

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,6 @@ description 'Installs/Configures nginx_conf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
 
-depends 'nginx'
-
 %w(ubuntu debian centos redhat amazon scientific oracle fedora).each do |os|
   supports os
 end

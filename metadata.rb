d34c32e168ec365cd6cc28f36742cd162cc00e54
Lloyd Philbrook <lloyd@tablexi.com>
2016-09-23 09:47:15 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sysadmins@tablexi.com'
 license 'GNU Public License 3.0'
 description 'Installs/Configures nginx_conf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '2.0.0'
 
 %w(ubuntu debian centos redhat amazon scientific oracle fedora).each do |os|
   supports os
